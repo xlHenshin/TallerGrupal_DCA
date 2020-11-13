2d43556ee05937094c3347435be26b85863e0d0a
xlHenshin <andreslayer117@gmail.com>
2020-11-13 12:43:19 -0500

    src folder

---
+++ src/view/Main.java
@@ -0,0 +1,26 @@
+package view;
+
+import processing.core.PApplet;
+
+public class Main extends PApplet {
+
+	public static void main(String[] args) {
+		
+		PApplet.main(Main.class.getName());
+	}
+	
+	public void settings() {
+		
+		size(1440, 810);
+	}
+	
+	public void setup() {
+		
+	}
+	
+	public void draw() {
+		
+		background(0);
+	}
+
+}
