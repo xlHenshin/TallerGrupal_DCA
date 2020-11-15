44f6a2c03b5d14616c22ff786fcd878ca116dc3f
angellucruz16 <angellucruz16@hotmail.com>
2020-11-14 20:58:09 -0500

    New class added Shopping

--- src/view/BuyScreen.java
+++ src/view/BuyScreen.java
@@ -1,5 +1,15 @@
 package view;
 
-public class BuyScreen {
+import processing.core.PApplet;
 
-}
+public class BuyScreen extends ScreenFather {
+
+		public BuyScreen (int posY,PApplet app) {
+			super (posY,app);
+			screen = app.loadImage("../resources/bill.png");
+			
+		} //CONSTRUCTOR
+		
+	
+	} //CLASS
+
