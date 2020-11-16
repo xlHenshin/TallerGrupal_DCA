5d3068e99f9544a320c012ed631908c631be6701
cristian <crisdamencast@gmail.com>
2020-11-16 16:19:45 -0500

    message

--- src/view/BuyScreen.java
+++ src/view/BuyScreen.java
@@ -1,16 +1,16 @@
 package view;
 
+
+
 import controlP5.ControlP5;
 import controlP5.Textfield;
-import controller.ControlBuy;
 import processing.core.PApplet;
 
 public class BuyScreen extends ScreenFather{
 
 	private int change=5;
-	private ControlBuy controlbuy;
 	private String [] input;
-	private String cardholder, cardnumber, mm, yy, cvv;
+	private String cardholder, mm, yy, cvv,cardnumber;
 	private ControlP5 cp5;
 	private boolean resultado;
 	private boolean error=false;
@@ -21,7 +21,6 @@ public class BuyScreen extends ScreenFather{
 		screen = app.loadImage("../resources/payment.png");
 		this.app=app;
 
-		controlbuy = new ControlBuy();
 		cp5 = new ControlP5(app);
 		input = new String [5];
 
@@ -33,6 +32,7 @@ public class BuyScreen extends ScreenFather{
 
 
 		cp5.hide();
+		
 
 		for (int i = 0; i < input.length; i++) {
 			cp5.addTextfield(input[i]).setPosition((app.width / 2) - 118, 329 + (i * 47)).setSize(235, 35)
@@ -51,46 +51,49 @@ public class BuyScreen extends ScreenFather{
 		}
 	}*/
 
-	public void button() {
+public void button() {
 
 		if (app.mouseX > 647 && app.mouseX < 647 + 137
-				&& app.mouseY > 40 && app.mouseY < 40 + 19) // 	InitialScreen Button 
+				&& app.mouseY > 40 && app.mouseY < 40 + 19) { // 	InitialScreen Button 
 			change = 0;
-
+			cp5.hide();
+			error = false;
+		}
 		if (app.mouseX > 825 && app.mouseX < 825 + 65
-				&& app.mouseY > 40 && app.mouseY < 40 + 19) // 	Contacts Button
+				&& app.mouseY > 40 && app.mouseY < 40 + 19) { // 	Contacts Button
 			change = 3;
-
+			cp5.hide();
+			error = false;
+		}
 		if (app.mouseX > 975 && app.mouseX < 975 + 45
-				&& app.mouseY > 40 && app.mouseY < 40 + 19) // Login Button
+				&& app.mouseY > 40 && app.mouseY < 40 + 19) { // Login Button
 			change = 1;
-
+			cp5.hide();
+			error = false;
+		}
 		if (app.mouseX > 1096 && app.mouseX < 1096 + 82
-				&& app.mouseY > 40 && app.mouseY < 40 + 19) // Register
+				&& app.mouseY > 40 && app.mouseY < 40 + 19) { // Register
 			change = 2;
-
+			cp5.hide();
+			error = false;
+		}
 		if (app.mouseX > 719 && app.mouseX < 719 + 121
 				&& app.mouseY > 626 && app.mouseY < 626 + 28) {
-			// 	continue Button
 			cardholder = cp5.get(Textfield.class, "Cardholder Name").getText();
 			cardnumber = cp5.get(Textfield.class, "Card Number").getText();
 			mm = cp5.get(Textfield.class, "MM").getText();
 			yy = cp5.get(Textfield.class, "YY").getText();
 			cvv = cp5.get(Textfield.class, "CVV").getText();
-			System.out.println(cardnumber.length());
-			for (int i = 0; i < input.length; i++) {
+			System.out.println("presionado");
 				if (isString(cardholder) && isNumeric(cardnumber) && isNumeric(mm) &&
 						isNumeric(yy)  && isNumeric(cvv)) {
-					if (cardnumber.length() > 14 && cardnumber.length()< 16 && mm.length() > 0 && mm.length()<2 && yy.length() > 2 && yy.length()<4
-							&& mm.length() > 1 && mm.length()<3) {
-						cardnumber = input[1];
-						mm=input[2];
-						yy=input[3];
-						cvv=input[4];
-						cardholder = input[0];				
+					if (cardnumber.length() == 16 && mm.length() == 2 && yy.length() == 4
+							&& cvv.length() == 3) {
+						System.out.println("presionado");
 						change = 4;
 						cp5.hide();
 						error = false;
+						errorLength = false;
 					}
 					else {
 						errorLength = true;
@@ -101,11 +104,14 @@ public class BuyScreen extends ScreenFather{
 					error = true;
 				}
 			}
-		}
+		
 
 		if (app.mouseX > 600 && app.mouseX < 600 + 100
-				&& app.mouseY > 626 && app.mouseY < 626 + 28) // 	cancel Button 
+				&& app.mouseY > 626 && app.mouseY < 626 + 28) {// 	cancel Button 
 			change = 0;
+			cp5.hide();
+			error = false;
+		}
 	}
 
 	public void errorMessage () {
@@ -114,7 +120,7 @@ public class BuyScreen extends ScreenFather{
 		isNumeric(mm);
 		isNumeric(yy); 
 		isNumeric(cvv);
-		if (errorLength=true) {
+		if (errorLength==true) {
 			app.fill(0);
 			app.text("las tarjetas deben presentan 16 digitos, los meses 2 digitos, los años 4 digitos y los CVV 3 digitos", 640, 120);
 		}
@@ -123,7 +129,7 @@ public class BuyScreen extends ScreenFather{
 	public boolean isNumeric(String cadena) {
 
 		try {// se controla una posible excepcion
-			Integer.parseInt(cadena);// si se logra guardar correctamente es un numero
+			Long.parseLong(cadena);// si se logra guardar correctamente es un numero
 			resultado = true;
 		} catch (NumberFormatException excepcion) {//si no se logra guardar correctamente ocurre lo siguente:
 			resultado = false;
